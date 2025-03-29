c5c42ce5bf74af73181cb832e4cfd9f22304369e
PyDevC <anuskcse@gmail.com>
2025-03-29 18:56:44 +0530

    added evalmodel

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
-from dataset.hate_explain import HateExplain
-from train import BERT, HateBert, Roberta, HateCheck
-from torch.optim import Adam
+from dataset.tdavidson import tdavidson_hate_offensive
+from datasets import load_dataset, Dataset
 from train.base import train_model
 from train.model import save_model, load_model
 from sklearn.metrics import accuracy_score, r2_score, precision_score
+from sklearn.model_selection import train_test_split
+import torch.optim as optim
+import torch.nn as nn
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, get_scheduler
 
-def train():
-    model = BERT.bert()
-    dataset = HateExplain("")
-    optimizer = Adam(model.parameters(), lr=0.0001)
-    model = train_model(model, dataset, optimizer)
-    save_model(model, "BERT_HateExplain.pt")
+def train(huggingface:str, stratify_column, model_name, device):
+    data = load_dataset(huggingface)
+    df = data["train"].to_pandas()
+
+    train_df, val_df = train_test_split(df, 
+                                        test_size=0.25, 
+                                        stratify=df[stratify_column], 
+                                        random_state=42
+                                        )
+
+    train_dataset = Dataset.from_pandas(train_df, preserve_index=False)
+    val_dataset = Dataset.from_pandas(val_df, preserve_index=False)
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    train_dataset = tdavidson_hate_offensive(train_dataset, tokenizer)
+    val_dataset = tdavidson_hate_offensive(val_dataset, tokenizer)
+
+
+    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=3)
+    model.to(device)
+
+    optimizer = optim.AdamW(model.parameters(), lr=2e-5)
+    lr_scheduler = get_scheduler("linear", optimizer=optimizer, num_warmup_steps=0)
+    criterion = nn.CrossEntropyLoss()
+
+    model = train_model(model, train_dataset, val_dataset, optimizer, lr_scheduler, criterion)
+    save_model(model, model_name)
+
 
 def test():
-    model = BERT.bert()
-    model = load_model(model, "BERT_HateExplain.pt")
-    X_test = HateExplain("").X_test
-    y_test = HateExplain("").y_test
-    results = model.predict(X_test)
     return results, y_test
 
 def results(results, y_test):
--- train/base.py
+++ train/base.py
@@ -1,8 +1,9 @@
 import torch
+from sklearn.metrics import accuracy_score, r2_score, precision_score, f1_score
 from torch.utils.data import DataLoader
 
 
-def train_model(model, train_dataset, optimzer):
+def train_model(model, train_dataset, val_dataset, optimzer, lr_scheduler, criterion):
     """trains model over several num_epoch
     parameters:
         model: base model or algorithm
@@ -13,10 +14,12 @@ def train_model(model, train_dataset, optimzer):
         model: trained model
     """
     train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False)
+
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model = model.to(device)
 
-    num_epoch = 25
+    num_epoch = 3
     for epoch in range(num_epoch):
         model.train()
         for batch in train_dataloader:
@@ -28,10 +31,38 @@ def train_model(model, train_dataset, optimzer):
 
             outputs = model(input_ids=input, attention_mask=attention_mask, labels=labels)
 
-            loss = outputs.loss
+            loss = outputs.logits
             loss.backward()
-
             optimzer.step()
         
+            lr_scheduler.step()
+
+            total_loss += loss.item()
+            preds = torch.argmax(outputs.logits, dim=1)
+            correct += (preds == labels).sum().item()
+            total += labels.size(0)
+
         print(f"Epoch {epoch+1}/{num_epoch} completed!")
+
+    evaluate_model(model, val_loader, criterion, device)
     return model
+
+def evaluate_model(model, val_loader, criterion, device):
+    model.eval()
+    preds, true_labels = [], []
+    total_loss = 0
+
+    with torch.no_grad():
+        for batch in val_loader:
+            input_ids, attention_mask, labels = batch["input_ids"].to(device), batch["attention_mask"].to(device), batch["labels"].to(device)
+            outputs = model(input_ids, attention_mask=attention_mask)
+            loss = criterion(outputs.logits, labels)
+            total_loss += loss.item()
+
+            preds.extend(torch.argmax(outputs.logits, dim=1).cpu().numpy())
+            true_labels.extend(labels.cpu().numpy())
+
+    acc = accuracy_score(true_labels, preds)
+    f1 = f1_score(true_labels, preds, average="weighted")
+    r2 = r2_score(true_labels, preds)
+    print(f"Validation Loss: {total_loss / len(val_loader):.4f}, Accuracy: {acc:.4f}, F1-score: {f1:.4f}, r2_score: {r2}")
